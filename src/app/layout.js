2dd094b82f5afeb7917ff4b3aec9a8bc76d23484
Marya Clara <mcmaryas@gmail.com>
2023-11-22 15:36:56 -0300

    tentar pegar do travel para Glocre8s

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,140 @@
-/* Funções não funcionais ex: textos, imagens...8*/
-
+import './globals.css'
+import styles from './styles.module.css'
+//import {Quicksand} from 'next/font/google'
+import Image from 'next/image';
+import facebook from '../../public/facebook.svg';
+import twitter from '../../public/twitter.svg';
+import link from '../../public/link.png';
+import palmeira from '../../public/Palms.png';
+import nuvem1 from '../../public/nuvem1.svg';
+import nuvem2 from '../../public/nuvem2.png';
+import circulo from '../../public/Ellipse 1.svg';
 
 export const metadata = {
-  title: 'Glocre8s',
-};
+  title: 'Travel Landing Page',
+}
 
-function cimaEsquerdo({}){
-  return (
-    <p style={{left: x, top: y}} className={styles.entrar}>Sing In<span className={styles.levar}>Track</span>Wish List</p>
+/*const quicksand = Quicksand({
+  subsets: ['latin'],
+  display: 'swap'
+})*/
+
+function Logo({x, y}){
+   return(
+    <p style={{left: x, top: y}}className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+
+   );
+}
+function Menu({children, x, y}){
+  return(
+<ul style= {{left: x, top:y}} className={styles.menu}>
+  {children.map((ItemMenu) =>(
+    <li key={ItemMenu.nome}>{ItemMenu}</li>
+  ))}
+  </ul>
+  );
+} 
+
+function RedesSociais({x, y}){
+  return(
+    <div style={{left: x, top: y}} className={styles.redesSociais}>
+      <Image src={facebook}/>
+      <Image src={twitter}/>
+      <Image src={link}/> 
+    </div>
+  )
+}
+
+function Palmeira({x, y}){
+  return(
+    <div style={{left: x, top: y}} className={styles.palmeira}>
+      <Image src={palmeira}/>
+      </div>
+  )
+}
+
+function Nuvem1({x,y}){
+  return(
+    <div style={{left: x, top: y}} className={styles.nuvens}>
+      <Image src={nuvem1}/>
+    </div>
+  )
+}
+
+function Nuvem2({x,y}){
+  return(
+    <div style={{left: x, top: y}} className={styles.nuvens}>
+      <Image src={nuvem2}/>
+    </div>
+  )
+}
+
+function Circulo({x,y}){
+  return(
+    <div style={{left: x, top: y}} className={styles.circulo}>
+      <Image src={circulo}/>
+    </div>
+  )
+}
+
+function ItemMenu({nome}){
+  return(
+    <p className={styles.ItemMenu}>{nome}</p>
   );
 }
+
+
+function NumeroTelefone({x, y, numero}){
+  return(
+    <div style={{left:x, top:y}} className={styles.celular}>
+         <Image className={styles.iconeNumero} width={19} height={19} src="/phone.png" alt='imdbffmn'/>
+      <p className= {styles.numero}>{numero}</p>
+    </div>
+  );
+}
+
+
+function CaixaTitulo({textin, textan, x, y}){
+  return(
+    <div style= {{left: x, top:y}} className={styles.caixaTitulo}>
+      <p className={styles.textoMaior}>{textin}</p>
+
+      <p className={styles.textoMenor}>{textan}</p>
+    </div>
+  );
+}
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <header>
-          <Logo x={588} y={233} />
-          <cimaEsquerdo x={3} y={45}/>
-          {/* Corrigindo para usar a importação correta: */}
+          <Logo x={135} y={53} ativo={{nome: 'fulano', idade:'25', aluno:''}}/>  
+          <Menu x={435} y={60}>
+            <ItemMenu nome="Home"/>
+            <ItemMenu nome="About"/>
+            <ItemMenu nome="Contact"/>
+            <ItemMenu nome="Blog"/>
+            <ItemMenu nome="Videos"/>
+          </Menu>
+          <NumeroTelefone x={1102} y={63} numero="+38(097)8849989"/>
+          {/* <ItemMenuIcone/> */}
         </header>
-      </body>
+          <main>
+          <CaixaTitulo x={135} y={238} textin={"Your perfect honeymoon"}
+          textan={"Mayami is more than a honeymoon destination – it’s a perfect romantic spot for couples who like  spending time in beach. "}/>
+          {children}
+          </main>
+          <footer>
+          <RedesSociais x={1175} y={843}/>
+            <Circulo x={1175} y={155}/>
+            <Nuvem1 x={568} y={600}/>
+            <Nuvem2 x={638} y={155}/>
+            <Palmeira x={638} y={155}/>
+          </footer>
+        </body>
+        
     </html>
-  );
+  )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,53 @@
-/* Funções de botões*/
-import Image from 'next/image';
-import Logomarca from '../public/logo.jpg';  // Certifique-se de que o caminho para a imagem está correto
+import styles from './page.module.css'
+import Image from 'next/image'
+import hawaii from '../../public/hawaii.png'
+import seta from '../../public/arrow-right.svg'
+import cyprus from '../../public/cyprus.png'
+import Bali from '../../public/bali.png'
+import { Children } from 'react'
 
-function Glocre({ x, y }) {
-    return (
-     <div className={StyleSheet.Glocre}/>
-     <Image src={Logomarca}/>
-    );
-  }
-  
-  export default function Home(){
-    return(
-        <>
-            <Glocre x={588} y={233} />
-        </>
-    )
-  }
+function Botao({x,y}){
+ return (
+    <button style={{left:x, top:y}} className={styles.botao}>Start</button>
+  )
+}
+
+function Card({localidade, imagem}){
+  return(
+    <div className={styles.card}>
+      <div className={styles.cabecalhoCard}>
+      <p className={styles.localidade}>{localidade}</p>
+
+      <Image src={seta}/>
+      </div>
+      <Image src= {imagem} className={styles.imagem}/>
+    </div>
+  )
+}
+
+function Cards({children, x, y}){
+  return(
+    <div style={{left: x, top:y}} className={styles.cards}>
+      {children}
+    </div>
+  )
+}
+
+function Social({children, x, y}){
+  return(
+    <div style={{left: x, top: y}} className={styles.social}>{children}</div>
+  )
+}
+
+export default function Home() {
+  return (
+    <>
+    <Botao x={135} y={601.72}/>
+    <Cards x={135} y={801}>
+      <Card localidade="Hawaii" imagem={hawaii}/>
+      <Card localidade="Cyprus" imagem={cyprus}/>
+      <Card localidade="Bali" imagem={Bali}/>
+    </Cards>
+    </>
+  )
+}
