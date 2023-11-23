43d98876eeca4c92863c5abfd2ea36c06c7f32af
Marya Clara <mcmaryas@gmail.com>
2023-11-23 14:04:37 -0300

    Coloquei a logo corretamente e parte do anuncio

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,18 @@
+// import Image from 'next/image'
 import styles from './page.module.css'
-import Image from 'next/image'
-import hawaii from '../../public/hawaii.png'
-import seta from '../../public/arrow-right.svg'
-import cyprus from '../../public/cyprus.png'
-import Bali from '../../public/bali.png'
-import { Children } from 'react'
 
-function Botao({x,y}){
- return (
-    <button style={{left:x, top:y}} className={styles.botao}>Start</button>
-  )
-}
-
-function Card({localidade, imagem}){
+function Anuncio ({x, y}){
   return(
-    <div className={styles.card}>
-      <div className={styles.cabecalhoCard}>
-      <p className={styles.localidade}>{localidade}</p>
-
-      <Image src={seta}/>
-      </div>
-      <Image src= {imagem} className={styles.imagem}/>
-    </div>
-  )
-}
-
-function Cards({children, x, y}){
-  return(
-    <div style={{left: x, top:y}} className={styles.cards}>
-      {children}
-    </div>
-  )
-}
-
-function Social({children, x, y}){
-  return(
-    <div style={{left: x, top: y}} className={styles.social}>{children}</div>
+    <h2 style={{left:x, top:y}} className={styles.anunciar}>
+      Creativity Is Life!
+    </h2>
   )
 }
 
 export default function Home() {
   return (
-    <>
-    <Botao x={135} y={601.72}/>
-    <Cards x={135} y={801}>
-      <Card localidade="Hawaii" imagem={hawaii}/>
-      <Card localidade="Cyprus" imagem={cyprus}/>
-      <Card localidade="Bali" imagem={Bali}/>
-    </Cards>
-    </>
+    <main>
+      <Anuncio x={237} y={275}/>
+    </main>
   )
 }
